911aacb2732ddf7c8e0929ef55092b580ac87c65
Michael Plasmeier <plaz@theplaz.com>
2013-04-25 01:37:49 +0000

    build system to build migration table

--- migrator.py
+++ migrator.py
@@ -4,6 +4,8 @@ import re
 db.create_db_conn()
 db.create_orig_db_conn()
 
+#process fingerprints aka visits
+
 #load all existing visits
 db.orig_cur.execute("SELECT id, cookie_id, signature, ip, ip34, timestamp FROM cookies;")
 numrows = int(db.orig_cur.rowcount)
@@ -13,7 +15,6 @@ numrows = int(db.orig_cur.rowcount)
 #ip is visit[3]
 #ip34 is visit[4]
 #timestamp is visit[5]
-
 
 #for i in range(numrows):
 for i in range(1):
@@ -25,19 +26,19 @@ for i in range(1):
     db.orig_cur.execute("SELECT * FROM fingerprint WHERE signature = %s;", visit[2].strip())
     fingerprint = db.orig_cur.fetchone()
     print fingerprint
+    print visit
     
-    '''
     #load into new table
     #drop count = fingerprint[8]
     db.cur.execute("INSERT INTO visit (id, cookie_id, js, cookie_enabled, user_agent, http_accept, plugins, fonts, timezone, video, "+
-                                                 "signature, supercookies,ua_h, ft_h, ha_h, pi_h, ip, ip34, timestamp) "+
+                                                 "signature, supercookies, ua_h, ft_h, ha_h, pi_h, ip, ip34, timestamp) "+
                                                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
                        (visit[0], visit[1], fingerprint[0], fingerprint[1], fingerprint[2], fingerprint[3], fingerprint[4], fingerprint[5],
-                        fingerprint[6], fingerprint[7], fingerprint[9], fingerprint[9], fingerprint[10], fingerprint[11], fingerprint[12], fingerprint[13], 
+                        fingerprint[6], fingerprint[7], fingerprint[9], fingerprint[10], fingerprint[11], fingerprint[12], fingerprint[13],
                         fingerprint[14], visit[3], visit[4], visit[5]));
                         
     print 'added visit: '+str(visit[0])
-    '''
+    
     
     #load software version table
     #load user agent = fingerprint[2]
@@ -51,6 +52,7 @@ for i in range(1):
     print useragent
     
     #match browsers
+        #insert into software table
     
     #look at plugins = fingerprint[4]
     #print fingerprint[4]
@@ -68,9 +70,12 @@ for i in range(1):
             print parts[0].strip()
             print parts[1].strip()
             #TODO: pull out version #
+            #load into software table
         else:
             print 'error'
             print parts
+            
+
     
 db.conn.commit()
 db.close_db_conn()
---
+++ migrator2.py
@@ -0,0 +1,56 @@
+import db
+import re
+
+db.create_db_conn()
+db.create_orig_db_conn()
+
+#process users aka migrations
+
+#load all existing visits
+db.orig_cur.execute("SELECT DISTINCT cookie_id FROM cookies;")
+numrows = int(db.orig_cur.rowcount)
+
+
+#for i in range(numrows):
+for i in range(1):
+    user = db.orig_cur.fetchone()
+    print user[0].strip()
+    
+    if user[0].strip() != "no cookies":
+    
+        db.cur.execute("SELECT * FROM visit WHERE cookie_id = %s;", user[0].strip())
+        fingerprints = db.cur.fetchall()
+        print len(fingerprints)
+        
+        #save pairs where time > 1 hr
+        if len(fingerprints) > 1: #if == 1 then just skip (visitor only came once)
+            i = 0
+            for fingerprint1 in fingerprints:
+                if i <= len(fingerprints) - 2:
+                    fingerprint2 = fingerprints[i+1]
+                    print 'compare '+str(fingerprint1[0])+' '+str(fingerprint2[0])
+                    print fingerprint1[18]
+                    print fingerprint2[18]
+                    if (fingerprint2[18] - fingerprint1[18]) > (60*60):
+                        
+                        
+                        #count fonts added
+                        fonts_added = 0
+                        
+                        #count fonts removed
+                        fonts_removed = 0
+                        
+                        #insert into table
+                        db.cur.execute("INSERT INTO migration (cookie_id, visit_from, visit_to, fonts_added, fonts_removed) "+
+                                                     "VALUES (%s, %s, %s, %s, %s)", 
+                           (user[0].strip(), fingerprint1[0], fingerprint2[0], fonts_added, fonts_removed));
+                            
+                        print 'added migration for user: '+str(user[0])
+                    else:
+                        print 'less than an hour'
+                i+=1
+
+    
+db.conn.commit()
+db.close_db_conn()
+db.close_orig_db_conn()
